10c2ee02064f5aad860341673cc4ee5299ddee62
Serena Shah-Simpson <sshah-simpson@cloudflare.com>
2025-01-08 11:26:52 -0500

    Adds file upload to upload page

--- apps/fileworker/src/routes/api/[...paths]/+server.ts
+++ apps/fileworker/src/routes/api/[...paths]/+server.ts
@@ -5,3 +5,4 @@ import type { RequestHandler } from '@sveltejs/kit'
 export const GET: RequestHandler = routeHandler
 export const PUT: RequestHandler = routeHandler
 export const DELETE: RequestHandler = routeHandler
+export const POST: RequestHandler = routeHandler
