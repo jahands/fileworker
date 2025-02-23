3eaed948f25501d94f63be32420604e6908152d4
Jacob Hands <jacob@gogit.io>
2025-02-23 17:13:10 -0600

    Rename /front -> /app

--- apps/fileworker/src/routes/+layout.svelte
+++ apps/fileworker/src/routes/+layout.svelte
@@ -10,16 +10,16 @@
 
 <div class="x-4 mx-auto" style:max-width="768px">
 	<Navbar let:hidden let:toggle>
-		<NavBrand href="/front/upload">
+		<NavBrand href="/app/upload">
 			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
 				File Worker
 			</span>
 		</NavBrand>
 		<NavHamburger on:click={toggle} />
 		<NavUl {activeUrl} {hidden}>
-			<NavLi href="/front/upload">Upload</NavLi>
-			<NavLi href="/front/download">Download</NavLi>
-			<NavLi href="/front/delete">Delete</NavLi>
+			<NavLi href="/app/upload">Upload</NavLi>
+			<NavLi href="/app/download">Download</NavLi>
+			<NavLi href="/app/delete">Delete</NavLi>
 		</NavUl>
 	</Navbar>
 
--- apps/fileworker/src/routes/+page.server.ts
+++ apps/fileworker/src/routes/+page.server.ts
@@ -1,5 +1,5 @@
 import { redirect } from '@sveltejs/kit'
 
 export function load() {
-	redirect(307, '/front')
+	redirect(307, '/app')
 }
